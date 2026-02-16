f993f136e100c53be358eb8852060d0dd8d4318f
elvesbd <elvesbd@yahoo.com>
2026-02-16 11:05:42 -0300

    feat: add IPaymentProcessor interface and payment models

---
+++ src/DesignPatternChallenge/Models/PaymentResult.cs
@@ -0,0 +1,8 @@
+namespace DesignPatternChallenge.Models;
+
+public class PaymentResult
+{
+    public bool Success { get; set; }
+    public string TransactionId { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+}
